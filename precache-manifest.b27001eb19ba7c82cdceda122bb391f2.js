cf22894a3386d6aec4e3dfa2462a8e60d4abc217
Chih Liao <support@mainmining.com>
2022-07-25 21:53:19 +0200

    Updates

---
+++ precache-manifest.b27001eb19ba7c82cdceda122bb391f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba93ab1bcdba78b46b65b0ac725bbafd",
+    "url": "/mainmining/index.html"
+  },
+  {
+    "revision": "a5ef28d5a04a6d91b193",
+    "url": "/mainmining/static/css/2.e393c75e.chunk.css"
+  },
+  {
+    "revision": "541a12d17ee7f12f44c6",
+    "url": "/mainmining/static/css/main.77821edf.chunk.css"
+  },
+  {
+    "revision": "a5ef28d5a04a6d91b193",
+    "url": "/mainmining/static/js/2.e8801fdf.chunk.js"
+  },
+  {
+    "revision": "03f381b986feb8d21be760e053b68a3c",
+    "url": "/mainmining/static/js/2.e8801fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541a12d17ee7f12f44c6",
+    "url": "/mainmining/static/js/main.9f608cb5.chunk.js"
+  },
+  {
+    "revision": "d3bd13f4bec47de76973",
+    "url": "/mainmining/static/js/runtime-main.36e1b40a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/mainmining/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/mainmining/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/mainmining/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/mainmining/static/media/slick.f97e3bbf.svg"
+  }
+]);
