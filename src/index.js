ee4d09601eb50422033b58dbff752129a7e5ee07
Chih Liao <support@mainmining.com>
2022-07-31 20:43:41 +0200

    first commit8

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
+// ReactDOM.render(
+//     <React.StrictMode>
+//       <HashRouter>
+//         <App />
+//       </HashRouter>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+
+  ReactDOM.render(
     <React.StrictMode>
-        <App />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={ <App /> }>
+          </Route>
+        </Routes>
+      </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
   );
