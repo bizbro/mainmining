56362266a634ae9c8efe922afc77eaefece65123
Chih Liao <support@mainmining.com>
2022-07-31 20:01:50 +0200

    first commit8

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,17 @@
+import Home from "./Home";
+import HotProducts from "../asicminers/HotProducts";
+import React from "react"
+import Categories from "./Categories"
+import "./Home.css"
+import SliderHome from "./Slider"
+
+const Home = () => {
+  return (
+    <>
+        <Home/>
+        <HotProducts/>
+    </>
+  )
+}
+
+export default Home
